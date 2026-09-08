6524af31df7779c0ee203751b6357ef3f6cc2637
pritam carpenter <dilipcarpenter543@gmail.com>
2026-09-08 11:34:40 +0530

    img refresh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -213,7 +213,7 @@ export default function App() {
       role: "Full Stack Developer",
       name: "Pritam carpenter",
       field: "React.js, Next.js & TypeScript Systems",
-      image: "e:\smu\frontend\public\images\pritam.jpg", // <--- Yahan bhi /images/ joda hai
+      image: "public/images/pritam.jpg", // <--- Yahan bhi /images/ joda hai
       badgeColor: "#0284c7",
       badgeBg: "#e0f2fe",
       borderColor: "#7dd3fc",
