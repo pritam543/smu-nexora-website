75dd7d4a6de612c5478d3325cafa18e9c635c9b9
pritam carpenter <dilipcarpenter543@gmail.com>
2026-09-09 23:21:47 +0530

    public

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -188,8 +188,8 @@ export default function App() {
     {
       title: "Founder",
       name: "Sakshi Pare",
-      image: "/images/founder.jpg", // <--- Yahan bhi /images/ aur naam founder.jpg kiya hai
-      quote: "Our vision is to engineer resilient, high-speed digital platforms that accelerate client businesses while fostering next-generation IT engineering talent in Indore.",
+      image: "public/images/founder.jpg", // <--- Yahan bhi /images/ aur naam founder.jpg kiya hai
+      quote: "Our vision is to engineer resilient, high-speed digital platforms that accelerate client businesses while fostering next-generation IT engineering talent in Indore..",
       badgeColor: "#db2777",
       badgeBg: "#fce7f3",
       borderColor: "#f798c9",
